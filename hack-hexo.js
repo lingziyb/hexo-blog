966a929690dea3b1a26984d2d88e351bec6f7f14
lingziyb <zhaolinglizll@qq.com>
2019-11-03 20:11:32 +0800

    fix  ali-oss absolute url  403

--- hack-hexo.js
+++ hack-hexo.js
@@ -3,5 +3,5 @@ const urlForHelper = require("hexo/lib/plugins/helper/url_for");
 
 require.cache[urlForHelperId].exports = function (path, options) {
     const url = urlForHelper.call(this, path, options);
-    return url === "/" ? url : url + "/index.html"
+    return url === "/" ? url : url + "index.html"
 }

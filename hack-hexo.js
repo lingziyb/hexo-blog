e1e9ae2fc53d326c6ce1b7377217f62366f51286
lingziyb <zhaolinglizll@qq.com>
2019-11-03 17:56:41 +0800

    fix ali-OSS url 403

---
+++ hack-hexo.js
@@ -0,0 +1,7 @@
+const urlForHelperId = require.resolve("hexo/lib/plugins/helper/url_for");
+const urlForHelper = require("hexo/lib/plugins/helper/url_for");
+
+require.cache[urlForHelperId].exports = function (path, options) {
+    const url = urlForHelper.call(this, path, options);
+    return url === "/" ? url : url + "/index.html"
+}
